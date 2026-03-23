3427eae44e07edf5b79864204a263766e2ccdda3
BlankRip <aniketh_g5@outlook.com>
2026-03-23 12:46:10 +0530

    Interaction system interfaces added, some feedback on plan from Syamjith

---
+++ Source/UE3CsTry/InteractorHandlingComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SphereComponent.h"
+#include "InteractorHandlingComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS(ClassGroup="Collision", Blueprintable, meta=(DisplayName="Interactor Handling", BlueprintSpawnableComponent))
+class UE3CSTRY_API UInteractorHandlingComponent : public USphereComponent
+{
+	GENERATED_BODY()
+	
+};
